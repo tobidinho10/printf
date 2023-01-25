7ca14d79c1ed2e975a57a70106c0bbe5d2fd37d8
ANIEBIERE <justinaumoh2@gmail.com>
2023-01-25 02:46:22 -0800

    handle print

--- handle_print.c
+++ handle_print.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * OB
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
@@ -47,6 +48,3 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
     }
   return (printed_chars);
 }
-
-
-
